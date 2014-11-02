674e40a3c6cbe09fd985b591779ef25de8844a51
edvinas <edvinas.stulpinas@gmail.com>
2014-11-02 21:23:48 +0200

    Coupon generation changes

--- app/code/community/ES/Newssubscribers/Model/Subscriber.php
+++ app/code/community/ES/Newssubscribers/Model/Subscriber.php
@@ -11,24 +11,28 @@ class ES_Newssubscribers_Model_Subscriber extends Mage_Newsletter_Model_Subscrib
         $model = Mage::getModel('salesrule/rule');
         $model->load(Mage::getStoreConfig('newsletter/coupon/roleid'));
         $massGenerator = $model->getCouponMassGenerator();
-        $massGenerator->setData(array(
-            'rule_id' => Mage::getStoreConfig('newsletter/coupon/roleid'),
-            'qty' => 1,
-            'length' => Mage::getStoreConfig('newsletter/coupon/length'),
-            'format' => Mage::getStoreConfig('newsletter/coupon/format'),
-            'prefix' => Mage::getStoreConfig('newsletter/coupon/prefix'),
-            'suffix' => Mage::getStoreConfig('newsletter/coupon/suffix'),
-            'dash' => Mage::getStoreConfig('newsletter/coupon/dash'),
-            'uses_per_coupon' => 1,
-            'uses_per_customer' => 1
-        ));
-        $massGenerator->generatePool();
-        $generated = $massGenerator->getGeneratedCount();
-        $latestCuopon = max($model->getCoupons());
-        $couponData = $latestCuopon->getData();
-        if ($generated != 1)
-            Mage::throwException($this->__('There was a problem with coupon generation.'));
+        $session = Mage::getSingleton('core/session');
+        try {
+            $massGenerator->setData(array(
+                'rule_id' => Mage::getStoreConfig('newsletter/coupon/roleid'),
+                'qty' => 1,
+                'length' => Mage::getStoreConfig('newsletter/coupon/length'),
+                'format' => Mage::getStoreConfig('newsletter/coupon/format'),
+                'prefix' => Mage::getStoreConfig('newsletter/coupon/prefix'),
+                'suffix' => Mage::getStoreConfig('newsletter/coupon/suffix'),
+                'dash' => Mage::getStoreConfig('newsletter/coupon/dash'),
+                'uses_per_coupon' => 1,
+                'uses_per_customer' => 1
+            ));
+            $massGenerator->generatePool();
+            $latestCuopon = max($model->getCoupons());
+        } catch (Mage_Core_Exception $e) {
+            $session->addException($e, $this->__('There was a problem with coupon: %s', $e->getMessage()));
+        }
+        catch (Exception $e) {
+            $session->addException($e, $this->__('There was a problem with coupon.'));
+        }
 
-        return $couponData['code'];
+        return $latestCuopon->getCode();
     }
 }
